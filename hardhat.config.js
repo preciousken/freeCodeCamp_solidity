e6cc9f32b3ebb5d4d38fce0922e7962b4ef7a83e
unknown <pkehinde27@gmail.com>
2023-08-30 13:59:58 +0100

    completed the hardhat simple storage application

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
 require('./tasks/block-number')
 require("@nomicfoundation/hardhat-verify");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COIN_MARKET_CAP = process.env.COIN_MARKET_CAP
 module.exports = {
   defaultNetwork:"hardhat",
   networks:{
@@ -24,6 +26,13 @@ module.exports = {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled:true,
+    outputFile:"gas-report.txt",
+    noColors:true,
+    currency:"USD",
+    coinmarketcap:COIN_MARKET_CAP
   },
   solidity: "0.8.8",
 };
